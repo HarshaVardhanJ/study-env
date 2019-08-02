ac84e9c9545bab08313c30f04435b640da085bd9
Jerry Smith <jdsmith1012@me.com>
2019-08-02 11:52:56 -0500

    Refactored a bit more

--- common/setup-services.sh
+++ common/setup-services.sh
@@ -3,17 +3,31 @@
 ISO=$1;
 NUM=$2;
 
+# Detect OS version
+VERSION=$(grep VERSION_ID /etc/os-release | cut -d\" -f2);
+
 # Mount ISO temporarily
 mount -o loop ~vagrant/iso/${ISO} /mnt;
 
 # Create local repository
 rm -rf /var/www/html/*;
-mkdir -p /var/www/html/BaseOS;
-cp -ar /mnt/BaseOS/Packages /var/www/html/BaseOS;
-createrepo -v /var/www/html/BaseOS;
-mkdir -p /var/www/html/AppStream;
-cp -ar /mnt/AppStream/Packages /var/www/html/AppStream;
-createrepo -v /var/www/html/AppStream;
+
+if [[ "$VERSION" == "7.0" ]]; then
+  # RHEL 7
+  mkdir -p /var/www/html;
+  cp -ar /mnt/Packages /var/www/html;
+  createrepo -v /var/www/html;
+else
+  # RHEL 8
+  mkdir -p /var/www/html/BaseOS;
+  cp -ar /mnt/BaseOS/Packages /var/www/html/BaseOS;
+  createrepo -v /var/www/html/BaseOS;
+  mkdir -p /var/www/html/AppStream;
+  cp -ar /mnt/AppStream/Packages /var/www/html/AppStream;
+  createrepo -v /var/www/html/AppStream;
+fi
+
+# Restore SELinux contexts
 restorecon -Rv /var/www/html;
 
 # Cleanup
@@ -59,3 +73,9 @@ systemctl start dnsmasq;
 
 # Set temporary hostname
 hostnamectl set-hostname services;
+
+# Setup FirewallD
+firewall-cmd --permanent --add-service http;
+firewall-cmd --permanent --add-service dns;
+firewall-cmd --permanent --add-service ntp;
+firewall-cmd --reload;
